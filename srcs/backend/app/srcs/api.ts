e6be4a6430a0413477b7658aaa4b3ac7f2bc4d6b
tissad <tissad@student.42.fr>
2025-07-24 14:57:24 +0200

    remove frontend build

--- srcs/backend/app/srcs/api.ts
+++ srcs/backend/app/srcs/api.ts
@@ -6,7 +6,7 @@
 /*   By: tissad <tissad@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/23 15:58:35 by tissad            #+#    #+#             */
-/*   Updated: 2025/07/23 16:03:45 by tissad           ###   ########.fr       */
+/*   Updated: 2025/07/24 14:53:49 by tissad           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@ import Fastify from 'fastify';
 import cors from '@fastify/cors';
 
 // Import the Fastify framework
-const fastify = Fastify({ logger: true });
+const app = Fastify({ logger: true });
 
 // Define the API endpoint for user signup
 interface SignupBody {
@@ -23,7 +23,7 @@ interface SignupBody {
 }
 
 // Register the POST route for user signup
-fastify.post<{ Body: SignupBody }>('/api', async (request, reply) => {
+app.post<{ Body: SignupBody }>('/api', async (request, reply) => {
   const { username, password } = request.body;
 
 
@@ -36,13 +36,21 @@ fastify.post<{ Body: SignupBody }>('/api', async (request, reply) => {
 });
 
 
+// Register CORS plugin to allow cross-origin requests  
+app.register(cors, {
+  origin: 'http://0.0.0.0:3000', // Allow specific origins
+  methods: ['GET', 'POST'], // Allow specific methods
+  credentials: true, // Allow credentials
+});
+
+
 // Start the Fastify server
 const start = async () => {
   try {
-    await fastify.listen({ port: 4000, host: '0.0.0.0' });
+    await app.listen({ port: 4000, host: '0.0.0.0' });
     console.log('🚀 Server is running at http://localhost:4000');
   } catch (err) {
-    fastify.log.error(err);
+    app.log.error(err);
     process.exit(1);
   }
 };
