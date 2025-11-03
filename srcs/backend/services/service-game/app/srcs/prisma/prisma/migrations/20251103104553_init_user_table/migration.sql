352cd5dc7ebe9c031f2ad0464c0b174cc6326992
tissad <tissad@student.42.fr>
2025-11-03 12:07:44 +0100

    setup redis and Prisma connection for database integration

---
+++ srcs/backend/services/service-game/app/srcs/prisma/prisma/migrations/20251103104553_init_user_table/migration.sql
@@ -0,0 +1,16 @@
+-- CreateTable
+CREATE TABLE "GameProfile" (
+    "id" TEXT NOT NULL,
+    "username" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "GameProfile_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "GameProfile_username_key" ON "GameProfile"("username");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "GameProfile_email_key" ON "GameProfile"("email");
