b948cb59c4a9985abd904c4e5e115c18fdb0833c
tissad <tissad@student.42.fr>
2025-11-21 14:25:57 +0100

    auto save

--- srcs/backend/services/service-users/app/srcs/internal-services-routes/internal-services/verifyToken.service.ts
+++ srcs/backend/services/service-users/app/srcs/internal-services-routes/internal-services/verifyToken.service.ts
@@ -6,13 +6,20 @@
 /*   By: tissad <tissad@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/21 12:43:37 by tissad            #+#    #+#             */
-/*   Updated: 2025/11/21 12:46:07 by tissad           ###   ########.fr       */
+/*   Updated: 2025/11/21 14:24:09 by tissad           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 import { FastifyInstance } from "fastify";
 import { JwtUtils } from "../../utils/jwt.utils";
 
-export async function verifyTokenService(tocken : string): Promise<boolean> {
-    const decoded = JwtUtils.verifyToken(tocken, process.env.INTERNAL_API_KEY!);
-    
+// fuction to verify token returns the payload if the token is valid
+export async function verifyTokenService(tocken : string): Promise<any> {
+    try {
+        const payload = JwtUtils.verifyAccessToken(tocken);
+        return payload;
+    }
+    catch (error) {
+        return null;
+    }
+}
