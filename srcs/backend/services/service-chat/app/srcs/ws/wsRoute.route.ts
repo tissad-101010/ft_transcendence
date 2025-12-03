a255cdf0d4af17f30c6dc9c3187a1af85c462a7f
tissad <tissad@student.42.fr>
2025-12-03 14:56:07 +0100

    Revert "auto save"
    This reverts commit d45dcd84792153067f90f9e480906485a65ee271.

--- srcs/backend/services/service-chat/app/srcs/ws/wsRoute.route.ts
+++ srcs/backend/services/service-chat/app/srcs/ws/wsRoute.route.ts
@@ -1,16 +1,11 @@
 import { FastifyInstance } from "fastify";
-import { FastifyRequest } from "fastify";
+
 import { handleWebSocketConnection } from "../ws/handleWebSocketConnection";
 import { WebSocket } from "ws";
 
-export async function wsRoutes(app: FastifyInstance, request: FastifyRequest) {
+export async function wsRoutes(app: FastifyInstance) {
 
-  // 1. get cookies from header
-  const cookies = request.headers.cookie;
-  const token = cookies
-    ?.split("; ")
-    .find((cookie) => cookie.startsWith("token="))
-    ?.split("=")[1];  
+
   // 2. Map des clients connectés
   const clients = new Map<number, WebSocket>();
 
@@ -18,6 +13,6 @@ export async function wsRoutes(app: FastifyInstance, request: FastifyRequest) {
   app.get(
     "/ws",
     { websocket: true },
-    handleWebSocketConnection(clients/* , token */)
+    handleWebSocketConnection(clients)
   );
 }
